234a5462f7221a0b2186bdceb1d9c830a26f421c
Vy <hintoken@gmail.com>
2021-05-20 16:24:06 +0700

    file data

--- QuanLyThuVien/src/GUI/MainMenu.java
+++ QuanLyThuVien/src/GUI/MainMenu.java
@@ -391,7 +391,14 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_btQLMuonTraActionPerformed
 
     private void btQLNhapHangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btQLNhapHangActionPerformed
-        // TODO add your handling code here:
+        QuanLyPhieuNhap QLPN = new QuanLyPhieuNhap();
+                try {
+                    QLPN.ListPN();
+                    QLPN.ListChiTiet();
+                } catch (Exception ex) {
+                    Logger.getLogger(QuanLySach.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                QLPN.setVisible(true);
     }//GEN-LAST:event_btQLNhapHangActionPerformed
 
     private void btThongKeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btThongKeActionPerformed
@@ -411,7 +418,18 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_btQLNhanVienActionPerformed
 
     private void btDanhMucActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btDanhMucActionPerformed
-        // TODO add your handling code here:
+         DanhMuc DM=new DanhMuc();
+            try {     
+                 DM.ListTheLoai();
+                 DM.ListTacGia();
+                 DM.ListNXB();
+                 DM.ListAccount();
+                 DM.ListLoiPhat();
+                 DM.ListNCC();
+             } catch (Exception ex) {
+                 Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
+             }
+            DM.setVisible(true);
     }//GEN-LAST:event_btDanhMucActionPerformed
 
     private void btQLPhieuPhatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btQLPhieuPhatActionPerformed
